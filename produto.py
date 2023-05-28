c919f0bc278ca8793db1eabcc90f9cc2342e9eb9
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-05-27 21:24:30 -0300

    crud da classe produto pronta

--- produto.py
+++ produto.py
@@ -26,29 +26,73 @@ class Table_Produto:
         for coluna in colunas_info:
             self.colunas.append(coluna[0])        
         
-    # lembrar de colocar alguns parametros opcionais
-    #colocar try except 
     def create(self, nome, preco, estoque, categoria, local_de_fabricacao, disponibilidade):
-        self.cursor.execute(f'''
-            INSERT INTO produto (nome, preco, estoque, categoria, local_de_fabricacao, disponibilidade)
-            VALUES ('{nome}', '{preco}', '{estoque}', '{categoria}', '{local_de_fabricacao}', '{disponibilidade}');
-        ''')
-        self.connection.commit()
-        print("insert foi")
+        try:
+            self.cursor.execute(f'''
+                INSERT INTO produto (nome, preco, estoque, categoria, local_de_fabricacao, disponibilidade)
+                VALUES ('{nome}', '{preco}', '{estoque}', '{categoria}', '{local_de_fabricacao}', '{disponibilidade}');
+            ''')
+            return self.connection.commit()
+        except:
+            return 0
+        
+    def read_by_id(self, id):
+        try:
+            self.cursor.execute(f'''
+            SELECT * FROM Produto WHERE id = {id};
+            ''')
+            return self.cursor.fetchone()
+        except:
+            return None
 
-    # try catch
-    def read(self, id):
-        self.cursor.execute(f'''
-        SELECT * FROM Produto WHERE id = {id};
-        ''')
-        return self.cursor.fetchone()
-
-    # fazer read por nome
+    def read_by_name(self, name):
+        try:
+            self.cursor.execute(f'''
+            SELECT * FROM Produto WHERE nome LIKE '%{name}%';
+            ''')
+            return self.cursor.fetchall()
+        except:
+            return None
+        
+    def read_all_available(self):
+        try:
+            self.cursor.execute(f'''
+            SELECT * FROM Produto WHERE disponibilidade = 1;
+            ''')
+            return self.cursor.fetchall()
+        except:
+            return None
+        
+    def read_all(self):
+        try:
+            self.cursor.execute(f'''
+            SELECT * FROM Produto;
+            ''')
+            return self.cursor.fetchall()
+        except:
+            return None
 
     def update(self, id, coluna, valor):
-        print(f'''
-        UPDATE Produto
-        SET {coluna} = {valor} WHERE id = {id};
-        '''
-        )
-        #print("update foi")
+        try:
+            if type(valor) == str:
+                self.cursor.execute(f'''
+                UPDATE Produto SET {coluna} = "{valor}" WHERE id = {id};
+                '''
+                )
+            else:
+                self.cursor.execute(f'''
+                UPDATE Produto SET {coluna} = {valor} WHERE id = {id};
+                '''
+                )
+            return self.connection.commit()
+        except:
+            return None
+        
+    def delete(self, id):
+        try: 
+            self.cursor.execute(f'''
+            UPDATE Produto SET disponibilidade = 0 WHERE id = {id};
+            ''')
+            return self.connection.commit()
+        except:
+            return None
