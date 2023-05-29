288439e5677d838e371eb0a6a9e7855ff2a0195c
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-05-29 11:47:27 -0300

    crud completo de compra_produto, testado

---
+++ t_compra_produto.py
@@ -0,0 +1,78 @@
+import mysql as ms
+
+class Table_Compra_Produto:
+    def __init__(self, connection):
+        self.connection = connection
+        self.cursor = connection.cursor()
+        self.cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS Produto_Compra(
+            id_compra INT NOT NULL,
+            id_produto INT NOT NULL,
+            quantidade INT NOT NULL,
+
+            CONSTRAINT PK_Produto_Compra PRIMARY KEY (id_compra,id_produto),
+            FOREIGN KEY (id_compra) REFERENCES Compra(id),
+            FOREIGN KEY (id_produto) REFERENCES Produto(id)
+            );
+        ''')
+        self.connection.commit()
+    
+    def create(self, id_compra, id_produto, quantidade):
+        try:
+            self.cursor.execute(f'''
+                INSERT INTO Produto_Compra(id_compra, id_produto, quantidade)
+                VALUES ({id_compra}, {id_produto}, {quantidade});
+                ''')
+            self.connection.commit()
+            return 1
+        except:
+            return 0
+    
+    def read(self, id_compra, id_produto):
+        try:
+            self.cursor.execute(f'''
+                SELECT quantidade FROM Produto_Compra WHERE id_compra = {id_compra} AND id_produto = {id_produto};
+                ''')
+            return self.cursor.fetchone()[0]
+        except:
+            return 0
+        
+    def read_all_from_compra(self, id_compra):
+        try:
+            self.cursor.execute(f'''
+                SELECT * FROM Produto_Compra WHERE id_compra = {id_compra};
+                ''')
+            return self.cursor.fetchall()
+        except:
+            return 0
+    
+    def update(self, id_compra, id_produto, quantidade):
+        try:
+            self.cursor.execute(f'''
+                UPDATE Produto_Compra SET quantidade = {quantidade} 
+                WHERE id_compra = {id_compra} AND id_produto = {id_produto};
+                ''')
+            self.connection.commit()
+            return 1
+        except:
+            return 0
+    
+    def delete(self, id_compra, id_produto):
+        try:
+            self.cursor.execute(f'''
+                DELETE FROM Produto_Compra WHERE id_compra = {id_compra} AND id_produto = {id_produto};
+                ''')
+            self.connection.commit()
+            return 1
+        except:
+            return 0
+    
+    def delete_all_from_compra(self, id_compra):
+        try:
+            self.cursor.execute(f'''
+                DELETE FROM Produto_Compra WHERE id_compra = {id_compra};
+                ''')
+            self.connection.commit()
+            return 1
+        except:
+            return 0
