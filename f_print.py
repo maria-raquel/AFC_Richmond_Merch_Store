69260f5d8201624348df87b562a441351264ac22
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-05-29 20:05:20 -0300

    funções de print (opções de menu e mensagens)

---
+++ f_print.py
@@ -0,0 +1,125 @@
+def boas_vindas():
+    print("----------------------------------------")
+    print("--Bem vindo ao controle de estoque da---")
+    print("----Loja oficial do A.F.C. Richmond-----")
+    print("------------WE ARE RICHMOND-------------")
+    print("----------------------------------------")
+
+def mensagem_1():
+    print("----------------------------------------")
+    print("-------------FÚTBALL IS LIFE------------")
+    print("----------------------------------------")
+
+def mensagem_2():
+    print("----------------------------------------")
+    print("-------------WE ARE RICHMOND------------")
+    print("----------------------------------------")
+
+def mensagem_3():
+    print("----------------------------------------")
+    print("------------------BELIEVE---------------")
+    print("----------------------------------------")
+
+def mensagem_4():
+    print("----------------------------------------")
+    print("-------------GO GREYHOUNDS--------------")
+    print("----------------------------------------")
+
+def mensagem_5():
+    print("----------------------------------------")
+    print("-------WE'RE RICHMOND TILL WE DIE-------")
+    print("----------------------------------------")
+
+def despedida():
+    print("----------------------------------------")
+    print("-----------Sistema encerrado------------")
+    print("-------------até a próxima--------------")
+    print("----------------------------------------")
+    print("--╔══╗ ╔═══╗╔╗   ╔══╗╔═══╗╔╗  ╔╗╔═══╗---")
+    print("--║╔╗║ ║╔══╝║║   ╚╣╠╝║╔══╝║╚╗╔╝║║╔══╝---")
+    print("--║╚╝╚╗║╚══╗║║    ║║ ║╚══╗╚╗║║╔╝║╚══╗---")
+    print("--║╔═╗║║╔══╝║║ ╔╗ ║║ ║╔══╝ ║╚╝║ ║╔══╝---")
+    print("--║╚═╝║║╚══╗║╚═╝║╔╣╠╗║╚══╗ ╚╗╔╝ ║╚══╗---")
+    print("--╚═══╝╚═══╝╚═══╝╚══╝╚═══╝  ╚╝  ╚═══╝---")
+    print("----------------------------------------")
+
+def menu_principal():
+    print("Menu: ")
+    print("1 - Compras")
+    print("2 - Produtos")
+    print("3 - Clientes")
+    print("4 - Vendedores")
+    print("5 - Relatórios")
+    print("0 - Sair")
+
+def menu_compra():
+    print("Menu - Compras: ")
+    print("1 - Realizar nova compra")
+    print("2 - Buscar compra já feita")
+    print("3 - Atualizar compra já feita")
+    print("4 - Cancelar compra já feita")
+    print("0 - Voltar")
+
+def submenu_compra_busca():
+    print("Menu - Compras - Busca: ")
+    print("1 - Por id")
+    print("2 - Por cliente")
+    print("3 - Por vendedor")
+    print("4 - Por data")
+    print("5 - Com pagamento pendente")
+    print("6 - Todas")
+    print("0 - Voltar")
+
+def menu_produto():
+    print("Menu - Produtos: ")
+    print("1 - Cadastrar novo produto")
+    print("2 - Buscar produto cadastrado")
+    print("3 - Atualizar produto cadastrado")
+    print("4 - Remover produto cadastrado")
+    print("0 - Voltar")
+
+def submenu_produto_busca():
+    print("Menu - Produtos - Busca: ")
+    print("1 - Por id")
+    print("2 - Por nome")
+    print("3 - Todos disponíveis")
+    print("4 - Todos")
+    print("0 - Voltar")
+
+def menu_cliente():
+    print("Menu - Clientes: ")
+    print("1 - Cadastrar novo cliente")
+    print("2 - Buscar cliente cadastrado")
+    print("3 - Atualizar cliente cadastrado")
+    print("4 - Remover cliente cadastrado")
+    print("0 - Voltar")
+
+def submenu_cliente_busca():
+    print("Menu - Clientes - Busca: ")
+    print("1 - Por id")
+    print("2 - Por nome")
+    print("3 - Por CPF")
+    print("4 - Todos")
+    print("0 - Voltar")
+
+def menu_vendedor():
+    print("Menu - Vendedores: ")
+    print("1 - Cadastrar novo vendedor")
+    print("2 - Buscar vendedor cadastrado")
+    print("3 - Atualizar vendedor cadastrado")
+    print("0 - Voltar")
+
+def submenu_vendedor_busca():
+    print("Menu - Vendedores - Busca: ")
+    print("1 - Por id")
+    print("2 - Por nome")
+    print("3 - Todos ativos")
+    print("4 - Todos")
+    print("0 - Voltar")
+
+def menu_relatorios():
+    print("Menu - Relatórios: ")
+    print("1 - Relatório de compras")
+    print("2 - Relatório de produtos")
+    print("3 - Relatório de clientes")
+    print("4 - Relatório de vendedores")
