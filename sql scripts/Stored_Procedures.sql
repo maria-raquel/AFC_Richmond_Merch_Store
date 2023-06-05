e822470e15e29b52cce33b9c524a979f1d3eb1f5
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-06-05 17:18:04 -0300

    novo stored procedure, de cancelamento de pagamento

--- sql scripts/Stored_Procedures.sql
+++ sql scripts/Stored_Procedures.sql
@@ -12,3 +12,20 @@ WHERE id = id_da_compra
 
 END $$
 DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE Cancelamento (IN id_da_compra INT)
+BEGIN
+
+UPDATE Pagamento
+SET status_do_pagamento = "Cancelado"
+WHERE id_compra = id_da_compra;
+
+UPDATE Compra 
+SET status_da_compra = "Cancelada"
+WHERE id = id_da_compra;
+
+END $$
+DELIMITER ;
