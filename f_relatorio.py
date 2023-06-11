2d5597cb492f6c68888ecf689bb09833eee0d6d7
maria-raquel <mariaraquelmartinez97@gmail.com>
2023-06-11 07:42:51 -0300

    organização

--- f_relatorio.py
+++ f_relatorio.py
@@ -15,8 +15,8 @@ Compra_Produto = cp.Table_Compra_Produto(connection)
 Cliente = cl.Table_Cliente(connection)
 
 meses = {'01': 'Janeiro', '02': 'Fevereiro', '03': 'Março', '04': 'Abril',
-             '05': 'Maio', '06': 'Junho', '07': 'Julho', '08': 'Agosto',
-             '09': 'Setembro', '10': 'Outubro', '11': 'Novembro', '12': 'Dezembro'}
+         '05': 'Maio', '06': 'Junho', '07': 'Julho', '08': 'Agosto',
+         '09': 'Setembro', '10': 'Outubro', '11': 'Novembro', '12': 'Dezembro'}
 
 def consultas(id, mes, ano):
     vendedor = Vendedor.read_by_id(id)
